749b24c54aa56207f8322d8d17c408896f9b4325
pierangelo1982 <pierangelo1982@gmail.com>
2020-10-10 14:48:58 +0200

    add text

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World \n');
+    res.end('Hello World - jenkins working great\n');
 });
 
 server.listen(port, hostname, () =>  {
